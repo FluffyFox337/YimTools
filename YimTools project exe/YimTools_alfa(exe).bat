8535f7065e165105f8e9736df15be44ece72fa93
~~~FluffyFox~~~ <84251725+FluffyFox337@users.noreply.github.com>
2024-05-02 12:04:16 +0400

    Update YimTools_alfa(exe).bat

--- YimTools project exe/YimTools_alfa(exe).bat
+++ YimTools project exe/YimTools_alfa(exe).bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDVdTw+bAE+1EbsQ5+n//Na+q0ApW+0za8/LyLuEJeMHqkD8cKoh2X1V1sIPA3s=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJgZksaHErQXA==
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQIJIRZgQwuOLws=
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDD2LMV+1CbkP1+n36evn
-::cRolqwZ3JBvQF1fEqQIEIBJHDBeBLmKqEvUR6eP+/KqOtg0YVfEmOJvUmqGeJOEH7wXle5Jt0nVNlscDDR8YRxumLiIxp3sCt2qBd8aYt0LuRFvJ4EQiW2Z7imXfnj86Yt9t2tYQ1m7e
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRma4VUlNns=
-::Zh4grVQjdCyDJGyX8VAjFDVdTw+bAE+1EbsQ5+n//Na+q0ApW+0za8/LyLuEJeMHqkD8cKoU33dul8QAHyRZchSqIAosrA4=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @ECHO OFF
 
 :: Set environment variables for source and destination paths
